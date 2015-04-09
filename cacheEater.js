91d3cfff1442e6fc505a0dd1fc4b4ab600c376df
nrakochy <nick.rakochy@gmail.com>
2015-04-09 15:50:30 -0500

    Made data removal a named function

--- cacheEater.js
+++ cacheEater.js
@@ -1,22 +1,24 @@
 var time = new Date().getTime();
 var milliSecondsPerDay = 1000 * 60 * 60 * 24;
-var oneWeekAgo = (time - (millisecondsPerDay * 7);
-var oneMonthAgo = (time - (millisecondsPerDay * 7 * 31);
-var oneYearAgo = (time - (millisecondsPerDay * 7 * 365);
+var oneWeekAgo = time - (millisecondsPerDay * 7);
+var oneMonthAgo = (time - (millisecondsPerDay * 31);
+var oneYearAgo = (time - (millisecondsPerDay * 365);
 
-chrome.browsingData.remove({
-  "since": oneYearAgo
-}, {
-  "appcache": true,
-  "cache": true,
-  "cookies": true,
-  "downloads": true,
-  "fileSystems": true,
-  "formData": true,
-  "history": true,
-  "indexedDB": true,
-  "localStorage": true,
-  "pluginData": true,
-  "passwords": true,
-  "webSQL": true
-});
+var removeData = function(){
+  chrome.browsingData.remove({
+    "since": oneYearAgo
+  }, {
+    "appcache": true,
+    "cache": true,
+    "cookies": true,
+    "downloads": true,
+    "fileSystems": true,
+    "formData": true,
+    "history": true,
+    "indexedDB": true,
+    "localStorage": true,
+    "pluginData": true,
+    "passwords": true,
+    "webSQL": true
+  });
+}
